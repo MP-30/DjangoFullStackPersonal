af143e5126e42eb555c0916c3021408b1d2d6921
adityamoney <adityabhadauriya93@gmail.com>
2024-02-20 21:47:11 +0530

    ecommerce app start

--- newproject/myapp/myapp/settings.py
+++ newproject/myapp/myapp/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website',
+    'ecommerceapp',
 ]
 
 MIDDLEWARE = [
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'myapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- newproject/myapp/myapp/views.py
+++ newproject/myapp/myapp/views.py
@@ -1,11 +1,6 @@
 from django.http import HttpResponse
 import datetime
 
-def test(request):
-  date = datetime.datetime.now()
-  print("test function is called from view")
-  return HttpResponse("<h1>Hello this is index page</h1>" + str(date))
-
 def about(request):
   return HttpResponse("<h1>This is about page</h1>")
 
